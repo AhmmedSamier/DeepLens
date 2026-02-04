84841819b61630be41609b32d5b05e6a6dbe1272
Ahmed Samir <ahmmedsamier2017@gmail.com>
2026-02-04 10:02:44 +0200

    Fix open file search scope updates

--- language-server/src/core/providers/symbol-provider.ts
+++ language-server/src/core/providers/symbol-provider.ts
@@ -18,7 +18,9 @@ export class SymbolProvider implements ISearchProvider {
             scope !== SearchScope.TYPES &&
             scope !== SearchScope.PROPERTIES &&
             scope !== SearchScope.ENDPOINTS &&
-            scope !== SearchScope.COMMANDS
+            scope !== SearchScope.COMMANDS &&
+            scope !== SearchScope.OPEN &&
+            scope !== SearchScope.MODIFIED
         ) {
             return [];
         }
