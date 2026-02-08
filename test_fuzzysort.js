427b64d3002622d5946bd4de3ab106b2c8c67076
Ahmed <ahmmedsamier2017@gmail.com>
2026-02-08 02:18:01 +0200

    cleanup

--- test_fuzzysort.js
+++
@@ -1,9 +0,0 @@
-const Fuzzysort = require('fuzzysort');
-
-const p = Fuzzysort.prepare('method2');
-const res = Fuzzysort.single('method', p);
-console.log('Score for "method" in "method2":', res ? res.score : 'null');
-
-const p2 = Fuzzysort.prepare('method');
-const res2 = Fuzzysort.single('method', p2);
-console.log('Score for "method" in "method":', res2 ? res2.score : 'null');
